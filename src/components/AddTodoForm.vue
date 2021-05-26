43b90cd9ebde95d741813bb07367137163145157
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-05-26 22:05:19 +0300

    = add checkboxes that are mandatory to check

--- src/components/AddTodoForm.vue
+++ src/components/AddTodoForm.vue
@@ -1,5 +1,15 @@
 <template>
   <form class="add-todo__form" id="add-todo-form" @submit.prevent="addTodo">
+    <div class="add-todo__form-item flex flex-row">
+      <label class="add-todo__form-control_label">
+        <input type="checkbox" v-model="licenseAgree" />
+        Accept license?
+      </label>
+      <label class="add-todo__form-control_label">
+        <input type="checkbox" v-model="receiveNews" />
+        Receive News?
+      </label>
+    </div>
     <div class="add-todo__form-item">
       <Input
         class="add-todo__form-control"
@@ -12,7 +22,7 @@
       <Button
         class="add-todo__form-submit"
         type="submit"
-        :disabled="title.length < 1"
+        :disabled="!submitAllowed"
         >Add</Button
       >
     </div>
@@ -32,6 +42,8 @@ export default {
   data: function () {
     return {
       title: "",
+      licenseAgree: true,
+      receiveNews: true,
     };
   },
   methods: {
@@ -40,17 +52,42 @@ export default {
       this.title = "";
     },
   },
+  computed: {
+    submitAllowed() {
+      const { title, licenseAgree, receiveNews } = this;
+
+      return (title.length > 0) & licenseAgree && receiveNews;
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .add-todo__form {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .add-todo__form-item {
+  &:not(:last-child) {
+    margin-bottom: 12px;
+  }
 }
 
 .add-todo__form-control {
+}
+
+.add-todo__form-control_label {
+  &:not(:first-child) {
+    margin-left: 12px;
+  }
+
+  input[type="checkbox"] {
+    margin: 0 4px 0 0;
+    vertical-align: -1px;
+  }
 }
 
 .add-todo__form-submit {
